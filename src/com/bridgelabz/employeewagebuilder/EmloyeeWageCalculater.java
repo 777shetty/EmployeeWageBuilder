6cf7e235316fbcf443b1c474583db208c0244a58
777shetty <shetty.chetu7@gmail.com>
2021-09-14 20:54:44 +0530

    UPDATE: Add wage details

--- src/com/bridgelabz/employeewagebuilder/EmloyeeWageCalculater.java
+++ src/com/bridgelabz/employeewagebuilder/EmloyeeWageCalculater.java
@@ -2,7 +2,17 @@ package com.bridgelabz.employeewagebuilder;
 
 public class EmloyeeWageCalculater {
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Program!");
+		int fullTime = 1;
+		int wagePerHour = 20;
+		int empHours = 0;
+		int empWage = 0;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == fullTime)
+			empHours = 8;
+		else
+			empHours = 0;
+		empWage = empHours * wagePerHour;
+		System.out.println("Emp Wage:" + empWage) ;
 	}
 }
 
