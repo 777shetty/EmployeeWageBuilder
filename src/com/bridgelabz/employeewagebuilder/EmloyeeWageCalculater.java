186b38b291a71f1f795bb4bac98de7be5885e016
777shetty <shetty.chetu7@gmail.com>
2021-09-15 19:22:20 +0530

    MODIFY: compute a wages for a max hours

--- src/com/bridgelabz/employeewagebuilder/EmloyeeWageCalculater.java
+++ src/com/bridgelabz/employeewagebuilder/EmloyeeWageCalculater.java
@@ -1,9 +1,33 @@
 package com.bridgelabz.employeewagebuilder;
 
 public class EmloyeeWageCalculater {
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUM_OF_WORKING_DAYS = 20;
+	public static final int MAX_HRS_IN_MONTH = 100;
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program!");
+		int empHours = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+			totalWorkingDays++;
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			switch (empCheck) {
+			case IS_FULL_TIME:
+				empHours = 8;
+				break;
+			case IS_PART_TIME:
+				empHours = 4;
+				break;
+			default:
+				empHours = 0;
+			}
+			totalEmpHrs += empHours;
+			System.out.println("Day " + totalWorkingDays + " - Hours worked: " + empHours);
+		}
+		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Total Emp Wage: " + totalEmpWage);
 	}
 }
-
 
